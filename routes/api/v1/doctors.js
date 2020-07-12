cc7486f75966f662a11ff20abd34139da28b1c58
rockingatgithub <contactwithsid@gmail.com>
2020-07-12 17:19:01 +0530

    controllers added

---
+++ routes/api/v1/doctors.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const doctorsController = require('../../../controllers/api/v1/doctors_controller');
+
+router.post('/register', )
+
+module.exports = router;
