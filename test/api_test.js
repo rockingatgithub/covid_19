537546e56aa3aa8fcee6c2c2ee51bc4357ece382
rockingatgithub <contactwithsid@gmail.com>
2020-07-27 01:25:49 +0530

    comments added

--- test/api_test.js
+++ test/api_test.js
@@ -1,27 +1,16 @@
+// ======================acquiring chai and other libraries======================
+
+
 const chai = require('chai');
 const should = require('should');
 const chaiHttp = require('chai-http')
 const server = require('../index');
 const { expect } = require('chai');
-let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjFkMzM0MDEyM2M3YzE0NzhjZjE3MjciLCJuYW1lIjoic3VkaGVuZHJhIiwiZW1haWwiOiJjeWJlcmtpbmdzaWRAZ21haWwuY29tIiwicGFzc3dvcmQiOiIxMjM0IiwiY3JlYXRlZEF0IjoiMjAyMC0wNy0yNlQwNzozOTo0NC4yODBaIiwidXBkYXRlZEF0IjoiMjAyMC0wNy0yNlQwNzozOTo0NC4yODBaIiwiX192IjowLCJpYXQiOjE1OTU3OTEzMDksImV4cCI6MTU5NTc5MjMwOX0.xNkHniFLPwlWWe9GaAtIQtkJhN399dAIhmNJV-DKmTc';
+let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjFkMzM0MDEyM2M3YzE0NzhjZjE3MjciLCJuYW1lIjoic3VkaGVuZHJhIiwiZW1haWwiOiJjeWJlcmtpbmdzaWRAZ21haWwuY29tIiwicGFzc3dvcmQiOiIxMjM0IiwiY3JlYXRlZEF0IjoiMjAyMC0wNy0yNlQwNzozOTo0NC4yODBaIiwidXBkYXRlZEF0IjoiMjAyMC0wNy0yNlQwNzozOTo0NC4yODBaIiwiX192IjowLCJpYXQiOjE1OTU3OTI3MTQsImV4cCI6MTU5NTc5MzcxNH0.4Vip4EyXvT6-oWvQidt3J5I_acJE00hC-yZwA4YK8Xk';
 chai.use(chaiHttp);
 
-// describe('Server', () => {
-//     it('it should GET all the reports', (done) => {
-//         chai.request('http://localhost:8000')
-//             .get('/api/v1/reports/allStatus/Travelled-Quarantine')
-//             .set({ "Authorization": `Bearer ${token}` })
-//             .end((err, res) => {
-//                 // console.log(res);
-//                 expect(res).to.have.status(200);
-//                 expect(res.body.message).to.equals('All data of a status');
-//                 expect(res.body.data.reports).to.be.a('array');
-//               done();
-//             });
-//       });
-    
-// }
-// )
+
+// ======================================checking the required routes first is patients register=======================
 
 
 
@@ -40,12 +29,13 @@ describe('Server', () => {
         expect(response).to.have.status(200);
         expect(response.body.message).to.equals('patient successfully registered');
         expect(response.body.patient).to.be.a('object');
-      //  done();    
       });
     
 }
 )
 
+
+// =====================================checking the new reports ==================================
 
 
 describe('Server', () => {
@@ -62,12 +52,13 @@ describe('Server', () => {
         expect(response).to.have.status(200);
         expect(response.body.message).to.equals('report successfully created');
         expect(response.body.report).to.be.a('object');
-      //  done();    
       });
     
 }
 )
 
+
+// ==================================testing the all reports route in patients=====================================
 
 
 describe('Server', () => {
@@ -78,7 +69,6 @@ describe('Server', () => {
         expect(response).to.have.status(200);
         expect(response.body.message).to.equals('all report for patient');
         expect(response.body.reports).to.be.a('array');
-      //  done();    
       });
     
 }
